39c7657c80950419023c4f1222e47eff105169aa
Hugo Salcedo <hugofebrero1@hotmail.com>
2026-03-07 14:14:18 -0500

    feat: add global exception handler for business and system errors

--- src/main/java/com/hugo/poliza_api/service/CoreMockService.java
+++ src/main/java/com/hugo/poliza_api/service/CoreMockService.java
@@ -3,6 +3,10 @@ package com.hugo.poliza_api.service;
 import com.hugo.poliza_api.dto.CoreEventoRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,19 +17,26 @@ public class CoreMockService {
 
     private final RestTemplate restTemplate;
 
+    @Value("${app.api-key}")
+    private String apiKey;
+
     public void registrarEvento(String evento, Long polizaId) {
-
-        log.info("Evento enviado al CORE -> evento: {}, polizaId: {}", evento, polizaId);
-
         CoreEventoRequest request = new CoreEventoRequest();
         request.setEvento(evento);
         request.setPolizaId(polizaId);
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("x-api-key", apiKey);
+
+        HttpEntity<CoreEventoRequest> entity = new HttpEntity<>(request, headers);
+
         restTemplate.postForObject(
                 "http://localhost:8080/core-mock/evento",
-                request,
+                entity,
                 String.class
         );
-    }
 
+        log.info("Evento enviado al CORE -> evento: {}, polizaId: {}", evento, polizaId);
+    }
 }
