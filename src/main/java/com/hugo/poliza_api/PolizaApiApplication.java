4b4dfb998c2663f412801e991f5043db926ca4cb
Hugo Salcedo <hugofebrero1@hotmail.com>
2026-03-07 09:11:02 -0500

    first commit

---
+++ src/main/java/com/hugo/poliza_api/PolizaApiApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.poliza_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolizaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolizaApiApplication.class, args);
+	}
+
+}
